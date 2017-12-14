68a4258a523333ee37aeb3741150a9ddc8b46f72
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-12-14 10:16:08 +0100

    Value has now a sign

--- src/main/groovy/com/scorsi/horilang/ast/Value.groovy
+++ src/main/groovy/com/scorsi/horilang/ast/Value.groovy
@@ -1,6 +1,7 @@
 package com.scorsi.horilang.ast
 
 import com.scorsi.horilang.Token
+import com.scorsi.horilang.TokenType
 import groovy.transform.ToString
 import org.jetbrains.annotations.NotNull
 
@@ -9,9 +10,15 @@ class Value extends Node {
 
     String value
     String type
+    Node sign = null
 
     @Override
     Node build(@NotNull List<Token> tokens, @NotNull List<Node> nodes) {
+        if (!nodes.isEmpty()) {
+            sign = nodes.last()
+            nodes.remove(nodes.lastIndexOf(sign))
+        }
+        println(nodes)
         value = tokens[0].value
         type = tokens[0].type.toString().toLowerCase().capitalize()
         return this
