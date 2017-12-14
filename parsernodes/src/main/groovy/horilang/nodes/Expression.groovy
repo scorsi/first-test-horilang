5914ff16e1d915eecede3cfc367a607da56353e7
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-12-14 17:35:29 +0100

    Add parser nodes

---
+++ parsernodes/src/main/groovy/horilang/nodes/Expression.groovy
@@ -0,0 +1,40 @@
+package horilang.nodes
+
+import groovy.transform.ToString
+import horilang.lexer.Token
+import org.jetbrains.annotations.NotNull
+
+@ToString(includeNames = true, includePackage = false, ignoreNulls = true)
+class Expression extends Node {
+
+    Node leftValue
+    Node operator = null
+    Node rightValue = null
+
+    @Override
+    Node build(@NotNull List<Token> tokens, @NotNull List<Node> nodes) {
+        def node = nodes.last()
+        if (node == null || !(node instanceof Value || node instanceof Expression))
+            throw new RuntimeException("Unexpected parameter, wanted [Expression, Value] but got $node")
+        rightValue = node
+        nodes.remove(nodes.lastIndexOf(node))
+
+        if (nodes.isEmpty()) {
+            leftValue = rightValue
+            rightValue = null
+            return this
+        }
+        node = nodes.last()
+        if (!node instanceof Operator)
+            throw new RuntimeException("Unexpected parameter, wanted [Operator] but got $node")
+        operator = node
+        nodes.remove(nodes.lastIndexOf(node))
+        node = nodes.last()
+        if (!node instanceof Value && !node instanceof Expression)
+            throw new RuntimeException("Unexpected parameter, wanted [Expression, Value] but got $node")
+        leftValue = node
+        nodes.remove(nodes.lastIndexOf(node))
+        return this
+    }
+
+}
---
+++ parsernodes/src/main/groovy/horilang/nodes/VariableDeclaration.groovy
@@ -0,0 +1,26 @@
+package horilang.nodes
+
+import groovy.transform.ToString
+import horilang.lexer.Token
+import org.jetbrains.annotations.NotNull
+
+@ToString(includeNames = true, includePackage = false, ignoreNulls = true)
+class VariableDeclaration extends Node {
+
+    String symbol
+    Node rightValue = null
+
+    @Override
+    Node build(@NotNull List<Token> tokens, @NotNull List<Node> nodes) {
+        if (!nodes.isEmpty()) {
+            def node = nodes.last()
+            if (!node instanceof Value && !node instanceof Expression)
+                throw new RuntimeException("Unexpected parameter, wanted [Expression, Value] but got $node")
+            rightValue = node
+            nodes.remove(nodes.lastIndexOf(node))
+        }
+        symbol = tokens[1].value
+        return this
+    }
+
+}
