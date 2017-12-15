46175af9f6ab1afc1f5414194d6d254dfe92806a
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-12-15 22:50:49 +0100

    Add FunctionDeclaration

---
+++ Compiler/parser/src/main/groovy/horilang/nodes/FunctionArguments.groovy
@@ -0,0 +1,41 @@
+package horilang.nodes
+
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+import horilang.lexer.Token
+import org.jetbrains.annotations.NotNull
+
+@ToString(includeNames = true, includePackage = false, ignoreNulls = true)
+@EqualsAndHashCode
+class FunctionArguments extends Node {
+
+    LinkedList<VariableDeclaration> arguments = new LinkedList<>()
+
+    FunctionArguments(@NotNull List<Token> tokens, @NotNull List<Node> nodes) {
+        super(tokens, nodes)
+        build(tokens, nodes)
+    }
+
+    private build(@NotNull List<Token> tokens, @NotNull List<Node> nodes) {
+        println(nodes)
+
+        def node = nodes.last()
+        if (node == null)
+            throw new RuntimeException("Unexpected parameter, wanted [FunctionArguments, VariableDeclaration] but got $node")
+
+        if (node instanceof FunctionArguments)
+            arguments = node.arguments
+        else if (node instanceof VariableDeclaration)
+            while (node != null && node instanceof VariableDeclaration) {
+                arguments.addFirst(node as VariableDeclaration)
+                nodes = nodes.dropRight(1)
+                if (nodes.isEmpty()) node = null
+                else node = nodes.last()
+            }
+        else
+            throw new RuntimeException("Unexpected parameter, wanted [FunctionArguments, VariableDeclaration] but got $node")
+
+        nodes = nodes.dropRight(1)
+    }
+
+}
---
+++ Compiler/parser/src/main/groovy/horilang/nodes/FunctionDeclaration.groovy
@@ -0,0 +1,49 @@
+package horilang.nodes
+
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+import horilang.lexer.Token
+import org.jetbrains.annotations.NotNull
+
+@ToString(includeNames = true, includePackage = false, ignoreNulls = true)
+@EqualsAndHashCode
+class FunctionDeclaration extends Node {
+
+    String symbol
+    FunctionArguments functionArguments
+    Node block
+
+    FunctionDeclaration(@NotNull List<Token> tokens, @NotNull List<Node> nodes) {
+        super(tokens, nodes)
+        build(tokens, nodes)
+    }
+
+    private build(@NotNull List<Token> tokens, @NotNull List<Node> nodes) {
+        symbol = tokens[1].value
+        def node = nodes.last()
+        if (node == null || !node instanceof Node)
+            throw new RuntimeException("Unexpected parameter, wanted [Block, Statement] but got $node")
+        block = node
+        nodes = nodes.dropRight(1)
+
+        if (nodes.isEmpty())
+            return
+        node = nodes.last()
+        if (node == null && !node instanceof FunctionArguments)
+            throw new RuntimeException("Unexpected parameter, wanted [Block] but got $node")
+        functionArguments = node as FunctionArguments
+        nodes = nodes.dropRight(1)
+    }
+
+    List<VariableDeclaration> getArguments() {
+        return functionArguments?.arguments
+    }
+
+    @Override
+    String toString() {
+        if (arguments == null || arguments.isEmpty())
+            return "FunctionDeclaration(symbol:$symbol, block:$block)"
+        else
+            return "FunctionDeclaration(symbol:$symbol, arguments:$arguments, block:$block)"
+    }
+}
